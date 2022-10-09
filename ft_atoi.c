00e6ce80982475d271372cce581fde98b3a4a782
Ayoub Taouaf <ataouaf@e1r6p11.1337.ma>
2022-10-09 23:53:10 +0100

    updating

---
+++ ft_atoi.c
@@ -0,0 +1,29 @@
+int ft_atoi(const char *str)
+{
+    int i = 0;
+    int r = 0;
+    int s = 1;
+    while((str[i] >= 9 && str[i] <= 13) || str[i] == 32 )
+        i++;
+    while (str[i] == '-' || str[i] == '+')
+    {
+        if(str[i] == '-')
+            s *= -1;
+        i++;
+    }
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        r = r * 10 + str[i] + 48;
+        i++;
+    }
+    return r*s;
+}
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	printf("%d\n",ft_atoi(" ---+--+1234ab567"));
+	printf("%d\n",atoi(" ---+--+1234ab567"));
+	printf("%d\n",ft_atoi("1234567"));
+	printf("%d\n",atoi("1234567"));
+}
---
+++ ft_memcpy.c
@@ -0,0 +1,4 @@
+void *ft_memcpy(void dst, const void src, size_t n)
+{
+    
+}
