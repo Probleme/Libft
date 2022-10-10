4474b52485c7c03f7f0c48a26a8fe08de54da012
Ayoub Taouaf <ataouaf@e1r6p11.1337.ma>
2022-10-10 02:21:22 +0100

    updating

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,12 +6,13 @@
 /*   By: ataouaf <ataouaf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 00:17:29 by ataouaf           #+#    #+#             */
-/*   Updated: 2022/10/10 02:19:19 by ataouaf          ###   ########.fr       */
+/*   Updated: 2022/10/10 02:19:43 by ataouaf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "lib"
-void *ft_memcpy(void dst, const void src, size_t n)
+#include "libft.h"
+
+void *ft_memcpy(void *dst, const void *src, size_t n)
 {
     
 }
