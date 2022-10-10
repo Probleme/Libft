13098972197b61dac4c9e42bc75983e4de9759ca
Probleme <ataouaf@e2r2p4.1337.ma>
2022-10-10 18:57:16 +0100

    updating

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: ataouaf <ataouaf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 00:18:27 by ataouaf           #+#    #+#             */
-/*   Updated: 2022/10/10 18:43:42 by ataouaf          ###   ########.fr       */
+/*   Updated: 2022/10/10 18:57:02 by ataouaf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,4 @@ void *ft_memset(void *b, int c, size_t len)
     while (len-- > 0)
         *ptr++ = c;
     return b;
-}
-int main()
-{
-    char a[] = "Ayoub";
-    printf("%s\n", ft_memset(a,98,5));
 }
