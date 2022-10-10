89a3f9ca923cca1d09086bcd31e883b5cd222b64
Ayoub Taouaf <ataouaf@e1r6p11.1337.ma>
2022-10-10 02:27:24 +0100

    updating

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ataouaf <ataouaf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 00:17:29 by ataouaf           #+#    #+#             */
-/*   Updated: 2022/10/10 02:19:43 by ataouaf          ###   ########.fr       */
+/*   Updated: 2022/10/10 02:27:21 by ataouaf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 
 void *ft_memcpy(void *dst, const void *src, size_t n)
 {
-    
+    char *d = dst;
+    const char *s = src;
+    while (n)
+        *d++ = *s++;
+    return dst;
 }
