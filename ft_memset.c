d716e3c309ee599f94b03aaa4a3d6322c66355a1
Ayoub Taouaf <ataouaf@e1r6p11.1337.ma>
2022-10-10 02:37:30 +0100

    updating

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: ataouaf <ataouaf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 00:18:27 by ataouaf           #+#    #+#             */
-/*   Updated: 2022/10/10 02:35:15 by ataouaf          ###   ########.fr       */
+/*   Updated: 2022/10/10 02:36:31 by ataouaf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void *ft_memset(void *b, int c, size_t len)
     i = 0;
     while (len < i)
     {
-        ((unsigned char *)b)[i] == (unsigned char )c;
+        ((unsigned char *)b)[i] = (unsigned char )c;
         i++;
     }
     return b;
@@ -26,5 +26,5 @@ void *ft_memset(void *b, int c, size_t len)
 int main()
 {
     char *a = "Ayoub";
-    printf("%s",ft_memset(a,97,2));
+    printf("%s",ft_memset(a,98,2));
 }
