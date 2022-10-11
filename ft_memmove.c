ee7ae5b7dfb26c82ac5b61b3b03cf57932e91bc4
Probleme <ataouaf@e1r5p2.1337.ma>
2022-10-11 05:05:03 +0100

    updating

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ataouaf <ataouaf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 21:39:22 by ataouaf           #+#    #+#             */
-/*   Updated: 2022/10/11 04:54:01 by ataouaf          ###   ########.fr       */
+/*   Updated: 2022/10/11 05:03:35 by ataouaf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,32 @@ void *ft_memmove(void *dst, const void *src, size_t len)
     char *d = dst;
     const char *s = src;
     if (d < s)
-    while (len--)
-      *d++ = *s++;
+        while (len--)
+            *d++ = *s++;
     else
     {
-      char *ls = s + (len-1);
+      const char *ls = s + (len-1);
       char *ld = d + (len-1);
-      while (len--)
-        *ld-- = *ls--;
+        while (len--)
+            *ld-- = *ls--;
     }
   return dst;
 }
+/*
 int main () 
 {
-   char dest[] = "oldstring";
-   const char src[]  = "newstring";
-   ft_memmove(dest, src, 9);
-   printf("After memmove dest = %s, src = %s\n", dest, src);
+   char s1[] = "anas jaidi";
+    char t1[255];
+    int i1 = -1;
+    char s[] = "anas jaidi";
+    char t[255];
+    int i = -1;
+
+    while (s[++i])
+        t[i + 4] = s[i];
+    while (s1[++i1])
+        t1[i1 + 4] = s1[i1];
+    printf("result : %s\n", memmove(t, t + 4, 10));
+    printf("result : %s\n", ft_memmove(t1, t1 + 4, 10));
 }
+*/
