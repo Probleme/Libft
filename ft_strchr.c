39b054161040c5441ad26823c2516bfe29583a93
Probleme <ataouaf@e1r8p3.1337.ma>
2022-10-13 23:03:53 +0100

    updating

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: ataouaf <ataouaf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 20:19:46 by ataouaf           #+#    #+#             */
-/*   Updated: 2022/10/13 22:28:16 by ataouaf          ###   ########.fr       */
+/*   Updated: 2022/10/13 22:35:01 by ataouaf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,11 @@
 
 char *ft_strchr(const char *s, int c)
 {
-    char c0 = c;
-    char *res;
     int i = 0;
-    while (s[i++] )
+    while (*s++ )
     {
-        if(s[i++] == c)
-            return ;
+        if(*s++ == c)
+            return *s++;
         else
             return NULL;
     }
@@ -28,5 +26,5 @@ char *ft_strchr(const char *s, int c)
 int main()
 {
     char ayoub[] = "Ayoub";
-    printf("%s",strchr())
+    printf("%s",strchr(ayoub,121));
 }
