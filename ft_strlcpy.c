b8886684c6cb393a94d1d3099acc4b5b3f6c7007
Probleme <ataouaf@e1r10p13.1337.ma>
2022-10-13 02:38:17 +0100

    updating

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: ataouaf <ataouaf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 06:16:01 by ataouaf           #+#    #+#             */
-/*   Updated: 2022/10/12 20:43:07 by ataouaf          ###   ########.fr       */
+/*   Updated: 2022/10/13 02:37:44 by ataouaf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,26 @@
 
  size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
  {
-    const char *s = src;
-    char *d = dst;
-    
+  size_t lsrc = ft_strlen(src);
+  
+  if (dstsize == 0)
+    return lsrc;
+  
+  while (*src && --dstsize)
+   *(dst++) = *(src++);
+  
+   return lsrc;
+ }
+ int main()
+ {
+  char s[] = "Ayoub";
+  char d[10] = {0};
+  printf("%zu\n",ft_strlcpy(d,s,0));
+  printf("%s\n",d);
+  
+  char s1[] = "Ayoub";
+  char d1[10] ;
+  printf("%zu\n",strlcpy(d1,s1, 0));
+  printf("%s\n",d1);
+  
  }
