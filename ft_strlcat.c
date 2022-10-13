b1fda89bacad5a6713ca6ac41f2cf4295ee0706d
Probleme <ataouaf@e1r7p14.1337.ma>
2022-10-13 08:57:26 +0100

    updating

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ataouaf <ataouaf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 05:33:49 by ataouaf           #+#    #+#             */
-/*   Updated: 2022/10/13 05:35:12 by ataouaf          ###   ########.fr       */
+/*   Updated: 2022/10/13 08:57:02 by ataouaf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,27 @@
 
 size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
+    size_t lsrc = ft_strlen(src);
+    size_t ldst = ft_strlen(dst);
+    if (dstsize == 0)
+        return lsrc;
+    if (dstsize > 0)
+        return lsrc + ldst;
     
+    while (*src && --dstsize)
+        *dst++ = *src++;
+    
+    return dstsize;
+}
+int main()
+{
+    char src[] = "Ayoub";
+    char dst[1] ;
+    printf("%zu\n",ft_strlcat(src,src+2,5));
+    printf("%s\n",src);
+    
+    char src1[] = "Ayoub";
+    char dst1[1] ;
+    printf("%zu\n",strlcat(src1,src1+2,5));
+    printf("%s\n",src1);
 }
