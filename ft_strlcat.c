1c88a9bc803f6df2e85b0edbec5ad7355e2984b3
Probleme <ataouaf@e1r7p14.1337.ma>
2022-10-13 09:59:35 +0100

    updating

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ataouaf <ataouaf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 05:33:49 by ataouaf           #+#    #+#             */
-/*   Updated: 2022/10/13 09:01:47 by ataouaf          ###   ########.fr       */
+/*   Updated: 2022/10/13 09:53:36 by ataouaf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,20 @@ size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
         return lsrc;
     if (ldst > dstsize)
         return lsrc + dstsize;
-    else
+    if (ldst < dstsize)
         return lsrc + ldst;
     while (*src && --dstsize)
         *dst++ = *src++;
     
     return dstsize;
 }
-int main()
-{
-    char src[] = "Ayoub";
-    char dst[1] ;
-    printf("%zu\n",ft_strlcat(src,src+2,5));
-    printf("%s\n",src);
+// int main()
+// {
+//     char src[] = "Ayoub Taouaf";
+//     printf("%zu\n",ft_strlcat(src, src+2, 0));
+//     printf("%s\n",src);
     
-    char src1[] = "Ayoub";
-    char dst1[1] ;
-    printf("%zu\n",strlcat(src1,src1+2,5));
-    printf("%s\n",src1);
-}
+//     char src1[] = "Ayoub Taouaf";
+//     printf("%zu\n",strlcat(src1, src1+2, 0));
+//     printf("%s\n",src1);
+// }
