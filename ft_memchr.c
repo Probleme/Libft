7d5ad47cc6e07927554484c092076c48d8ffcc55
Probleme <ataouaf@e1r12p3.1337.ma>
2022-10-17 10:48:10 +0100

    updating

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,34 +6,37 @@
 /*   By: ataouaf <ataouaf@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 06:58:37 by ataouaf           #+#    #+#             */
-/*   Updated: 2022/10/17 08:45:13 by ataouaf          ###   ########.fr       */
+/*   Updated: 2022/10/17 09:52:53 by ataouaf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-    size_t i = 0;
-    const unsigned char *str = s;
-    while (str[i] && n > i)
-    {
-        if(str[i] == (char) c)
-            return (unsigned char *)&str[i];
-        i++;
-    }
-    return 0;
+	size_t		i;
+	const char	*str;
+
+	i = 0;
+	str = s;
+	while (n > i)
+	{
+		if (str[i] == (char) c)
+			return ((unsigned char *)&str[i]);
+		i++;
+	}
+	return (0);
 }
 // int main()
 // {
-    // char a[] = "Ayoub";
-    // printf("%s\n",memchr(a,'o',6));
-    // char a1[] = "Ayoub";
-    // printf("%s",ft_memchr(a1,'o',6));
-    
+	// char a[] = "Ayoub";
+	// printf("%s\n",memchr(a,'o',6));
+	// char a1[] = "Ayoub";
+	// printf("%s",ft_memchr(a1,'o',6));
+
 // 	int tab[7] = {-49, 49, 1, -1, 0, -2, 2};
 // 	printf("%s\n", (char *)memchr(tab, -1, 7));
-    
+
 // 	int tab1[7] = {-49, 49, 1, -1, 0, -2, 2};
 // 	printf("%s", (char *)ft_memchr(tab1, -1, 7));
 // }
--- ft_memmove.c
+++ ft_memmove.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ataouaf <ataouaf@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ataouaf <ataouaf@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 21:39:22 by ataouaf           #+#    #+#             */
-/*   Updated: 2022/10/12 00:16:42 by ataouaf          ###   ########.fr       */
+/*   Updated: 2022/10/17 09:31:41 by ataouaf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ void *ft_memmove(void *dst, const void *src, size_t len)
 {
     char *d = dst;
     const char *s = src;
-    
+    if(!dst && !src)
+        return NULL;
     if (d < s)
         while (len--)
             *(d++) = *(s++);
@@ -25,37 +26,35 @@ void *ft_memmove(void *dst, const void *src, size_t len)
             *(d + len) = *(s + len);
   return dst;
 }
-/*
-int main () 
-{
-    char s1[10] = "Abde llah";
-    char t1[255];
-    int i1 = -1;
+
+// int main () 
+// {
+//     char s1[10] = "Abde llah";
+//     char t1[255];
+//     int i1 = -1;
 
     
-    char s[10] = "Abde llah";
-    char t[255];
-    int i = -1;
+//     char s[10] = "Abde llah";
+//     char t[255];
+//     int i = -1;
 
-    while (s[++i])
-        t[i + 4] = s[i];
-    printf("result : %s\n", memmove(t, t + 4, 10));
+//     while (s[++i])
+//         t[i + 4] = s[i];
+//     printf("result : %s\n", memmove(t, t + 4, 10));
         
-    while (s1[++i1])
-        t1[i1 + 4] = s1[i1];
-    printf("result : %s\n", ft_memmove(t1, t1 + 4, 10));
-}
-*/
-/*
-int main()
-{
-    int s1[4] = {10,10,10,10};
+//     while (s1[++i1])
+//         t1[i1 + 4] = s1[i1];
+//     printf("result : %s\n", ft_memmove(t1, t1 + 4, 10));
+// }
 
-    int d1[10]; 
-    int d[10];
-    ft_memmove(d1,s1,4);
-    ft_memmove(d,s1,4);
-    printf("the value is : %d\n",d1[0]);
-    printf("the value is : %d\n",d[0]);
-}
-*/
+// int main()
+// {
+//     int s1[] = {10,10,10,10};
+
+//     int d1[10]; 
+//     int d[10];
+//     ft_memmove(d1,s1,4);
+//     ft_memmove(d,s1,4);
+//     printf("the value is : %d\n",d1[0]);
+//     printf("the value is : %d\n",d[0]);
+// }
