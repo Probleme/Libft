1f87e5b18df42a0ade1f60a9cf1ac33a60a0b7c2
Probleme <ataouaf@e1r12p3.1337.ma>
2022-10-17 09:18:06 +0100

    updating

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ataouaf <ataouaf@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 00:17:29 by ataouaf           #+#    #+#             */
-/*   Updated: 2022/10/17 09:16:02 by ataouaf          ###   ########.fr       */
+/*   Updated: 2022/10/17 09:17:56 by ataouaf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,12 @@ void *ft_memcpy(void *dst, const void *src, size_t n)
     }
     return dst;
 }
-int main()
-{
-    char src[] = "Ayoub";
-    char dst[] = "";
-    char src1[] = "Ayoub";
-    char dst1[] = "";
-    printf("%s\n",memcpy(dst,src,5));
-    printf("%s\n",ft_memcpy(dst1,src1,5));
-}
+// int main()
+// {
+//     // char src[] = "Ayoub";
+//     // char dst[] = "";
+//     char src1[] = "Ayoub";
+//     char dst1[] = "";
+//     // printf("%s\n",memcpy(dst,src,3));
+//     printf("%s\n",ft_memcpy(dst1,src1,3));
+// }
