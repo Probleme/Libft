655eecd4e7b32c290a2b70f764da1ee46d3e752c
Probleme <ataouaf@e1r12p3.1337.ma>
2022-10-17 12:36:58 +0100

    updating

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: ataouaf <ataouaf@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 11:01:32 by ataouaf           #+#    #+#             */
-/*   Updated: 2022/10/17 11:40:49 by ataouaf          ###   ########.fr       */
+/*   Updated: 2022/10/17 12:35:14 by ataouaf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,29 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t				i;
-	const unsigned char	*str1;
-	const unsigned char	*str2;
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
 	i = 0;
-	str1 = s1;
-	str2 = s2;
-	while (str1[i] && str2[i] && i < n - 1)
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (i < n)
+	{
+		if (str1[i] == str2[i] && i == n - 1)
+			return (0);
+		if (!(str1[i] == str2[i]))
+			return (str1[i] - str2[i]);
 		i++;
-	return ((unsigned char) str1[i] - (unsigned char) str2[i]);
+	}
+	return (0);
 }
-int main()
-{
-	char a[] = "t\200";
-	char b[] = "t\0";
-	printf("%d\n",ft_memcmp(a,b,2));
-	char a1[] = "t\200";
-	char b1[] = "t\0";
-	printf("%d",memcmp(a1,b1,2));
-}
+// int main()
+// {
+// 	char a[] = "t\200";
+// 	char b[] = "t\0";
+// 	printf("%d\n",ft_memcmp(a,b,2));
+// 	char a1[] = "t\200";
+// 	char b1[] = "t\0";
+// 	printf("%d",memcmp(a1,b1,2));
+// }
