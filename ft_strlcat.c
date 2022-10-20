51157765fdba675e23086aa90f49243b84c299ef
Probleme <ataouaf@e1r11p5.1337.ma>
2022-10-21 00:40:54 +0100

    updating

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ataouaf <ataouaf@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 11:07:34 by ataouaf           #+#    #+#             */
-/*   Updated: 2022/10/20 05:10:03 by ataouaf          ###   ########.fr       */
+/*   Updated: 2022/10/21 00:40:19 by ataouaf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,34 +16,37 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	lsrc;
 	size_t	ldst;
-	int	i = 0;
+	size_t	i;
+	size_t	r;
 
+	if (!dst && dstsize == 0)
+		return (0);
+	i = 0;
 	lsrc = ft_strlen(src);
 	ldst = ft_strlen(dst);
-	if (dstsize == 0)
-		return (lsrc);
 	if (ldst > dstsize)
-		return (lsrc + dstsize);
-	else if (ldst < dstsize)
-		return (lsrc + ldst);
-	while (src[i] && --dstsize)
+		r = lsrc + dstsize;
+	else
+		r = lsrc + ldst;
+	while (src[i] && ldst < dstsize - 1 && dstsize)
 	{
-		dst[i] = src[i];
+		dst[ldst] = src[i];
+		ldst++;
 		i++;
 	}
-	*dst = '\0';
-	return (dstsize);
+	dst[ldst] = '\0';
+	return (r);
 }
 
 // int main()
 // {
 // 	char src[] = "Ayoub Taouaf";
-// 	char dst[11] = "";
-// 	printf("%zu\n",strlcat(dst, src, 11));
-// 	printf("%s\n",dst);
+// 	// char dst[11] = "";
+// 	printf("%zu\n",strlcat(0, src, 0));
+// 	printf("%s\n",src);
 
 //  	char src1[] = "Ayoub Taouaf";
-// 	char dst1[11] = "";
-// 	printf("%zu\n",ft_strlcat(dst1, src1, 11));
-// 	printf("%s\n",dst1);
+// 	// char dst1[11] = "";
+// 	printf("%zu\n",ft_strlcat(0, src1, 0));
+// 	printf("%s\n",src1);
 // }
